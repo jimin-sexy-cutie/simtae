c63acfb70d1592ae162def4abdbd01cd81f56d5c
jimin-sexy-cutie <79678365+jimin-sexy-cutie@users.noreply.github.com>
2021-06-04 10:41:18 +0900

    2번째 커밋

--- common.js
+++ common.js
@@ -1,7 +1,52 @@
+var ansArr = [];
+var massage;
+
+
 $(".q1").css("display","none");
+$(".q2").css("display","none");
+$(".q3").css("display","none");
+$(".q4").css("display","none");
+$(".q5").css("display","none");
+$(".q6").css("display","none");
+$(".q7").css("display","none");
 
 function fn_start(){
 	$(".main").css("display","none");
 	$(".q1").css("display","block");
 
 }
+
+function fn_next(page,val){
+	var next = page+1;
+	$(".q"+page).css("display","none");
+	$(".q"+next).css("display","block");
+	ansArr.push(val);
+
+	if(page == 5){
+		fn_test();
+	}
+}
+
+
+function fn_test(){
+	setTimeout(function(){
+		$(".q6").css("display","none");
+		$(".q7").css("display","block");
+		fn_result();
+	}, 1500);
+}
+
+function fn_result(){
+	
+	massage = ansArr[0]+"<br>"
+		+ansArr[1]+"<br>"
+			+ansArr[2]+"<br>"
+				+ansArr[3]+"<br>"
+					+ansArr[4]+"<br>";
+	$("#message").html(massage);
+}
+
+
+
+
+
